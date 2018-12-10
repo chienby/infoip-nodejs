cee119cc4d553f826f9f19bebfbca713c64178ed
Chien Nguyen <deirpes@gmail.com>
2018-12-10 11:32:50 +0700

    Install request ip library to get client ip

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const geoip = require('geoip-lite');
 const express = require('express');
+const geoip = require('geoip-lite');
+const requestIp = require('request-ip');
 const app = express();
 const port = process.env.PORT || '3000';
 
+app.use(requestIp.mw());
+
 app.get('/', (req, res) => {
-    let ip = req.ip;
+    let ip = req.clientIp;
     return res.json({ ip });
 });
 
