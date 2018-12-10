10eecdd63335a0d263d181359de79cfcc45e11ff
Chien Nguyen <gaamus@mail.com>
2018-12-10 11:27:55 +0700

    Fixed wrong env port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const geoip = require('geoip-lite');
 const express = require('express');
 const app = express();
-const port = process.env.port || '3000';
+const port = process.env.PORT || '3000';
 
 app.get('/', (req, res) => {
     let ip = req.ip;
